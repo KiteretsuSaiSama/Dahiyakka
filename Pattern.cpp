6abd8d1a3a3930b1d63b80a6b33986894b31bcc6
KiteretsuSaiSama <72118035+KiteretsuSaiSama@users.noreply.github.com>
2020-09-30 20:13:57 +0530

    Create Pattern.cpp
      *
       * *
      *   *
     *     *
    *********

---
+++ Pattern.cpp
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PATTERN
+{
+     public static void main(String[] args)
+        {
+           
+                System.out.println("");
+            }
+            sc.close();
+        }
+    }
