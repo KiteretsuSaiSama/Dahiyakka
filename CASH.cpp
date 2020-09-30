9d54a609678b84249fd21f042c5d5794ace7e04d
dorakit <72117270+dorakit@users.noreply.github.com>
2020-10-01 01:24:10 +0530

    Update CASH.cpp
    Code enhanced and snippets added

--- CASH.cpp
+++ CASH.cpp
@@ -2,7 +2,20 @@
 using namespace std;
  int main()
 {
-   //complete the program
+    long int t,n; cin>>t;
+    while(t--)
+    { long long int s=0;
+        long int k,r=0;
+        cin>>n;
+        cin>>k;
+        long int a[n];
+        for(long int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            s+=a[i];
+        }
+        r=s%k;
+        cout<<r<<endl;
     }
     return 0;
 }
