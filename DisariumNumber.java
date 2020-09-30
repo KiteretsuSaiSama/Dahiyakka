64fbdffc97201bba11120bb76214d6d51a44644a
KiteretsuSaiSama <72118035+KiteretsuSaiSama@users.noreply.github.com>
2020-09-30 17:07:08 +0530

    Create DisariumNumber.java
    improve the program to make it suitable for execution

---
+++ DisariumNumber.java
@@ -0,0 +1,27 @@
+//package missing
+public class Example11 {
+
+    public static void main(String args[]);
+        {
+            Scanner sc = new Scanner(System.in)
+            System.out.print("Input a number : ");
+            int num = sc.nextInt();
+            int copy = num, d = 0, sum = 0;
+            String s ;  //num is converted to string
+            int len = s.length();  
+             
+            while(copy>0)
+            {
+                d = copy % 10;  
+                sum = sum + (int)Math.pow(d,len);
+                len--;
+                copy = copy / 10;
+            }
+             
+            if(sum == num)
+                System.out.println("Disarium Number.");
+            else
+                System.out.println("Not a Disarium Number.");
+        }
+    }
+//= Integer.toString(num)
